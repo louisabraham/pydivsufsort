04aed00eb059ba5b0e5bd7b288681e871d3037f7
Louis Abraham <louis.abraham@yahoo.fr>
2020-02-27 16:38:01 +0100

    add example for #6

--- examples/example.py
+++ examples/example.py
@@ -6,13 +6,27 @@ import numpy as np
 from pydivsufsort import divsufsort
 
 
-random_string = np.random.randint(256, size=10_000_000, dtype=np.uint8)
-
 print(divsufsort(b"banana"))
 print(divsufsort(array.array("B", [98, 97, 110, 97, 110, 97])))
 print(divsufsort(np.array([98, 97, 110, 97, 110, 97]).astype(np.uint8)))
 
+
+n = 1_000_000
+
+random_string = np.random.randint(255, size=n, dtype=np.uint8)
+
 d = time()
-for _ in range(2):
-    divsufsort(random_string)
+divsufsort(random_string)
+print(time() - d)
+
+random_string = np.random.randint(255, size=n, dtype=np.uint64)
+
+d = time()
+divsufsort(random_string)
+print(time() - d)
+
+random_string[0] = -1
+
+d = time()
+divsufsort(random_string)
 print(time() - d)
