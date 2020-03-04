e4d3c55cd42ff50c107fc9b14220260749edad0a
Louis <louis.abraham@yahoo.fr>
2020-03-04 15:01:55 +0100

    test bash

---
+++ test.py
@@ -0,0 +1,10 @@
+
+from pathlib import Path
+from subprocess import Popen
+
+
+
+script = Path(__file__).parent / "test.sh"
+path = str(script.absolute()).replace("\\", r"\\")
+print(path)
+Popen(["bash.exe", "-c", path]).wait()
