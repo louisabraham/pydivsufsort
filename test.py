ba9576510913fb47caafd361a53d15102d3cd45c
Louis Abraham <louis.abraham@yahoo.fr>
2020-03-04 17:59:46 +0100

    exclude only amd64 builds

--- test.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from subprocess import Popen
-
-
-script = Path(__file__).parent / "test.sh"
-path = str(script.absolute()).replace("\\", r"\\")
-print(path)
-Popen(["bash.exe", "-c", path]).wait()
