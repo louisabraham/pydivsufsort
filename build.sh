c40c253b0a710cea79f5a84af38abc273d615d3a
Louis <louis.abraham@yahoo.fr>
2020-03-04 11:51:42 +0100

    ls

--- build.sh
+++ build.sh
@@ -9,8 +9,16 @@ rm -rf tempbuild
 mkdir tempbuild
 cd tempbuild
 cmake -DBUILD_DIVSUFSORT64=ON -DBUILD_EXAMPLES=OFF -DUSE_OPENMP=ON ../libdivsufsort
-ls
 cmake --build . --config Release
+
+echo "-----"
+ls lib
+echo "-----"
+ls lib/Release
+echo "-----"
+ls lib/examples/Release
+echo "-----"
+
 cd ..
 # copy the two largest files, aka the dll of libdivsufsort and libdivsufsort64
 mv $(du tempbuild/lib/libdivsufsort* | sort -nr | head -n2 | cut -f2) pydivsufsort
