f8fd882d014b41cdcb40165c28cb915cc05b6826
Louis <louis.abraham@yahoo.fr>
2020-03-04 16:23:29 +0100

    debug TypeError: LoadLibrary() argument 1 must be str, not WindowsPath

--- pydivsufsort/dll.py
+++ pydivsufsort/dll.py
@@ -5,8 +5,9 @@ import platform
 PATH = Path(__file__).parent
 
 if platform.system() == "Windows":
-    PATH_LIBDIVSUFSORT = next(PATH.glob("divsufsort.dll"))
-    PATH_LIBDIVSUFSORT64 = next(PATH.glob("divsufsort64.dll"))
+    PATH_LIBDIVSUFSORT = str(next(PATH.glob("divsufsort.dll")))
+    PATH_LIBDIVSUFSORT64 = str(next(PATH.glob("divsufsort64.dll")))
+    print(PATH_LIBDIVSUFSORT, PATH_LIBDIVSUFSORT64)
 else:
     PATH_LIBDIVSUFSORT = next(PATH.glob("libdivsufsort.*"))
     PATH_LIBDIVSUFSORT64 = next(PATH.glob("libdivsufsort64.*"))
