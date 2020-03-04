01873331ae06141bbf406bf54706e5fd763355d5
Louis <louis.abraham@yahoo.fr>
2020-03-04 16:43:11 +0100

    try to escape backslash

--- pydivsufsort/dll.py
+++ pydivsufsort/dll.py
@@ -9,8 +9,8 @@ print(platform.system())
 print("----------")
 
 if platform.system() == "Windows":
-    PATH_LIBDIVSUFSORT = str(next(PATH.glob("divsufsort.dll")))
-    PATH_LIBDIVSUFSORT64 = str(next(PATH.glob("divsufsort64.dll")))
+    PATH_LIBDIVSUFSORT = str(next(PATH.glob("divsufsort.dll"))).replace("\\", r"\\")
+    PATH_LIBDIVSUFSORT64 = str(next(PATH.glob("divsufsort64.dll"))).replace("\\", r"\\")
     print(PATH_LIBDIVSUFSORT, PATH_LIBDIVSUFSORT64)
 else:
     PATH_LIBDIVSUFSORT = next(PATH.glob("libdivsufsort.*"))
