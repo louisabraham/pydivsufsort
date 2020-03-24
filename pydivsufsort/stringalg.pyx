e10ba20e580b8992f9e3c5b1a7ada04297514c8c
Louis Abraham <louis.abraham@yahoo.fr>
2020-03-24 21:24:55 +0100

    remove warning for kasai with bytes input

--- pydivsufsort/stringalg.pyx
+++ pydivsufsort/stringalg.pyx
@@ -13,7 +13,8 @@ ctypedef fused sa_t:
     np.int64_t
 
 ctypedef fused string_t:
-    np.uint8_t
+    # handled separately in kasai_bytes
+    # np.uint8_t
     np.uint16_t
     np.uint32_t
     np.uint64_t
@@ -49,17 +50,39 @@ def _kasai(string_t[::1] s not None, sa_t[::1] sa not None):
 
     return lcp
 
-def kasai(s, sa=None):
-    if isinstance(s, bytes):
-        # bad, makes a copy
-        # fixing would probably require using ctypes
-        # like for divsufsort
-        # but it would prevent the use of the buffer interface
-        warnings.warn("Passing bytes to kasai() forces memory copy")
-        s = bytearray(s)
-    if sa is None:
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            sa = divsufsort(s)
+def _kasai_bytes(const char[::1] s not None, sa_t[::1] sa not None):
+    """Same as _kasai but with a const first argument.
+    Cython does not support const fused types arguments YET
+    but it will be fixed in the 0.30 release
+    <https://github.com/pandas-dev/pandas/issues/31710>
+    """
+        
+    cdef sa_t i, j, n, k
+    cdef np.ndarray[sa_t, ndim=1] lcp = np.empty_like(sa) 
+    cdef np.ndarray[sa_t, ndim=1] rank = np.empty_like(sa)
+        
+    n = len(sa)
 
+    for i in range(n):
+        rank[sa[i]] = i
+
+    k = 0
+    for i in range(n):
+        if rank[i] == n - 1:
+            lcp[n-1] = k = 0
+            continue
+        j = sa[rank[i] + 1]
+        while i + k < n and j + k < n and s[i+k] == s[j+k]:
+            k += 1
+        lcp[rank[i]] = k
+        if k:
+            k -= 1
+
+    return lcp
+
+def kasai(s, sa=None):
+    if sa is None:
+        sa = divsufsort(s)
+    if isinstance(s, bytes):
+        return _kasai_bytes(s, sa)
     return _kasai(s, sa)
