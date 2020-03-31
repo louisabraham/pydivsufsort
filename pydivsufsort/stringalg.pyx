d7ab4c7ad8b736c3255d8260317cbb8f64c27f8d
Louis Abraham <louis.abraham@yahoo.fr>
2020-04-01 00:02:12 +0200

    implement LCP queries (done)

--- pydivsufsort/stringalg.pyx
+++ pydivsufsort/stringalg.pyx
@@ -23,9 +23,25 @@ ctypedef fused string_t:
     np.int64_t
 
 
+def handle_input(s):
+    if isinstance(s, np.ndarray):
+        # in my tests, converting to contiguous and using [::1]
+        # gives *slightly* better performance than using [:]
+        if not s.flags["C_CONTIGUOUS"]:
+            # Make a contiguous copy of the numpy array.
+            s = np.ascontiguousarray(s)
+    if isinstance(s, str):
+        try:
+            s = s.encode("ascii")
+            if len(s) > 999:
+                warnings.warn("converting str argument uses more memory")
+        except UnicodeEncodeError:
+            raise TypeError("str must only contain ascii chars")
+    return s
+
+
 def _kasai(string_t[::1] s not None, sa_t[::1] sa not None):
 
-        
     cdef sa_t i, j, n, k
     cdef np.ndarray[sa_t, ndim=1] lcp = np.empty_like(sa) 
     cdef np.ndarray[sa_t, ndim=1] rank = np.empty_like(sa)
@@ -80,34 +96,25 @@ def _kasai_bytes(const unsigned char[::1] s not None, sa_t[::1] sa not None):
     return lcp
 
 def kasai(s, sa=None):
-    if isinstance(s, str):
-        try:
-            s = s.encode("ascii")
-            if len(s) > 999:
-                warnings.warn("converting str argument uses more memory")
-        except UnicodeEncodeError:
-            raise TypeError("str must only contain ascii chars")
-    if isinstance(s, np.ndarray):
-        # in my tests, converting to contiguous and using [::1]
-        # gives slightly better performance than using [:]
-        if not s.flags["C_CONTIGUOUS"]:
-            # Make a contiguous copy of the numpy array.
-            s = np.ascontiguousarray(s)
+    s = handle_input(s)
     if sa is None:
         sa = divsufsort(s)
     if isinstance(s, bytes):
         return _kasai_bytes(s, sa)
     return _kasai(s, sa)
 
-def lcp_segtree(string_t[::1] s not None, np.ndarray[sa_t, ndim=1] sa not None, lcp):
-    
-    cdef sa_t i, j, n, k
-    n = len(sa)
+def _lcp_segtree(
+        string_t[::1] s not None,
+        np.ndarray[sa_t, ndim=1] sa not None,
+        np.ndarray[sa_t, ndim=1] lcp not None
+    ):
 
-    # warning overflow, convert to unsigned?
+    cdef sa_t i, j, n, k
+    
     cdef np.ndarray[sa_t, ndim=1] segtree = np.concatenate([np.empty_like(lcp), lcp])
-    cdef np.ndarray[sa_t, ndim=1] rank = np.empty_like(sa)
+    cdef np.ndarray rank = np.empty_like(sa)
         
+    n = len(sa)
     for i in range(n-1, 0, -1):
         segtree[i] = min(segtree[i << 1], segtree[i << 1 | 1])
     
@@ -116,28 +123,54 @@ def lcp_segtree(string_t[::1] s not None, np.ndarray[sa_t, ndim=1] sa not None, 
 
     return rank, segtree
 
-def lcp_query(np.ndarray[sa_t, ndim=1] rank not None, sa_t[::1] segtree not None, queries):
-    cdef sa_t n, q, i, l, r
+
+def lcp_segtree(s, sa=None, lcp=None):
+    s = handle_input(s)
+    if sa is None:
+        sa = divsufsort(s)
+    if lcp is None:
+        lcp = kasai(s, sa)
+    if isinstance(s, bytes):
+        # tofix
+        s = bytearray(s)
+    return _lcp_segtree(s, sa, lcp)
+
+
+def lcp_query(
+        np.ndarray[sa_t, ndim=1] rank not None,
+        np.ndarray[sa_t, ndim=1] segtree not None,
+        queries
+    ):
+    cdef sa_t n, q, i
+
+    # note: l and r could hold 2*n-1
+    # fortunately, n is at most int64
+    cdef unsigned long long l, r
 
     n = len(rank)
     q = len(queries)
-    cdef np.ndarray[sa_t, ndim=1] ans = np.empty(q, dtype=rank.dtype)
 
+    cdef np.ndarray[sa_t, ndim=1] ans = np.empty(q, dtype=segtree.dtype)
+
+    # TODO: parallelize
+    # prange throws a lot of errors
     for i in range(q):
-        l, r = rank[queries[i]]
+        l, r = queries[i]
+        res = n - max(l, r)
+        l, r = rank[l], rank[r]
         if r < l:
             l, r = r, l
-        res = n
-        # print(l, r)
         l += n
         r += n
         while l < r:
             if l&1:
-                res = min(res, segtree[l])
+                if segtree[l] < res:
+                    res = segtree[l]
                 l += 1
             if r&1:
                 r -= 1
-                res = min(res, segtree[r])
+                if segtree[r] < res:
+                    res = segtree[r]
             l >>= 1 
             r >>= 1 
         ans[i] = res
