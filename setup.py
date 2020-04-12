d6f81fd315399e35ff9d27638e004590ff7a1b76
Louis Abraham <louis.abraham@yahoo.fr>
2020-04-12 21:17:51 +0200

    version 0.0.5

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ extensions = [
 
 setup(
     name="pydivsufsort",
-    version="0.0.4",
+    version="0.0.5",
     author="Louis Abraham",
     license="MIT",
     author_email="louis.abraham@yahoo.fr",
